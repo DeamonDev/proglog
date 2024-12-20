3f63b0bb08d26c491b8f1bf275bca0263dd9d40e
deamondev <piotr.rudnicki94@protonmail.com>
2024-12-20 20:54:02 +0100

    new method for store type

--- internal/log/store.go
+++ internal/log/store.go
@@ -15,9 +15,47 @@ const (
 	lenWidth = 0
 )
 
+/*
+The store is a simple wrapper around a file with two APIs to append
+and read bytes to and from a file.
+*/
 type store struct {
 	*os.File
 	mu   sync.Mutex
 	buf  *bufio.Writer
 	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
+}
+
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+
+	//We write the length of the record so that, when we read the record, we know how
+	//many bytes to read.
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, nil
+	}
+	w += lenWidth
+	s.size += uint64(w)
+
+	return uint64(w), pos, nil
 }
